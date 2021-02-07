141ee642b91c58de8de92f0a5b042a898052f04f
mrunhap <liubolovelife@gmail.com>
2021-02-07 23:20:02 +0800

    update git ignore

--- hackernews/visited-links.el
+++
@@ -1,1 +0,0 @@
-((hackernews-link . #s(hash-table size 65 test eql rehash-size 1.5 rehash-threshold 0.8125 data ())) (hackernews-comment-count . #s(hash-table size 65 test eql rehash-size 1.5 rehash-threshold 0.8125 data ())))
