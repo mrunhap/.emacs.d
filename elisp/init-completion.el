d46664b5b617958cfa50a8234ef46171cb696e61
mrunhap <liubolovelife@gmail.com>
2021-02-08 19:44:28 +0800

    add ivy-xref

--- elisp/init-completion.el
+++ elisp/init-completion.el
@@ -40,4 +40,19 @@
   :init
   (counsel-mode 1))
 
+(use-package ivy-xref
+  :straight
+  (ivy-xref :type git
+            :host github
+            :repo "alexmurray/ivy-xref")
+  :init
+  ;; xref initialization is different in Emacs 27 - there are two different
+  ;; variables which can be set rather than just one
+  (when (>= emacs-major-version 27)
+    (setq xref-show-definitions-function #'ivy-xref-show-defs))
+  ;; Necessary in Emacs <27. In Emacs 27 it will affect all xref-based
+  ;; commands other than xref-find-definitions (e.g. project-find-regexp)
+  ;; as well
+  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
+
 (provide 'init-completion)
