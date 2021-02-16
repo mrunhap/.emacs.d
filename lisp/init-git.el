9213cb40986aed51035f928b88ec4dd0287afebb
mrunhap <liubolovelife@gmail.com>
2021-02-16 23:29:54 +0800

    update

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -4,11 +4,4 @@
   :straight t
   :commands magit)
 
-(leaf dired-git-info
-  :straight t
-  :bind ((:dired-mode-map
-          ("v" . dired-git-info-mode)))
-  :custom
-  (dgi-auto-hide-details-p . nil))
-
 (provide 'init-git)
