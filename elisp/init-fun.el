a774da8f4450cd435e38e5411a0051b467fbb95f
mrunhap <liubolovelife@gmail.com>
2021-02-16 11:59:54 +0800

    update

--- elisp/init-fun.el
+++ elisp/init-fun.el
@@ -44,4 +44,12 @@
                     [simple-query "en.cppreference.com"
                                   "en.cppreference.com/mwiki/index.php?search=" ""]))))
 
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
 (provide 'init-fun)
