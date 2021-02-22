1f14aa74be765a09c23cbfcef86cfacb5ccb8cbf
mrunhap <liubolovelife@gmail.com>
2021-02-22 18:33:36 +0800

    update pytohn

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,3 +1,14 @@
 ;;; -*- lexical-binding: t -*-
 
+(leaf python
+  :tag "builtin"
+  :init
+  ;; Disable readline based native completion
+  (setq python-shell-completion-native-enable nil))
+
+(leaf highlight-indentation
+  :straight t
+  :hook
+  (python-mode-hook . highlight-indentation-mode))
+
 (provide 'init-python)
