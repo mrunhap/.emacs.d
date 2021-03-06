ac522253ba4c14285091de68ef7594693034341d
mrunhap <liubolovelife@gmail.com>
2021-03-06 11:21:54 +0800

    set *font* const

--- lisp/init-const.el
+++ lisp/init-const.el
@@ -16,4 +16,29 @@
   (eq system-type 'gnu/linux)
   "Are we running on a GNU/Linux system?")
 
+(defconst *font*
+  (cl-loop for font in '("Operator Mono Lig" "SF Mono"  "Fira Code"
+                          "DejaVu Sans Mono" "Consolas")
+           when (font-installed-p font)
+           return font)
+  "Font used for gui")
+
+(defconst *font-unicode*
+  (cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
+           when (font-installed-p font)
+           return font)
+  "Font used for display unicode")
+
+(defconst *font-cn*
+  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+           when (font-installed-p font)
+           return font)
+  "Font used for display chinese")
+
+(defconst *font-height*
+  (cond (sys/mac-x-p 130)
+        (sys/win32p 110)
+        (t 110))
+  "Font height")
+
 (provide 'init-const)
