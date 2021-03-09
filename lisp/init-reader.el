73213d2c4b7f221319ae0b8aa38160868a952c24
mrunhap <liubolovelife@gmail.com>
2021-03-09 20:21:16 +0800

    update

--- lisp/init-reader.el
+++ lisp/init-reader.el
@@ -1,30 +1,5 @@
 ;;; -*- lexical-binding: t -*-
 
-(leaf newsticker
-  :tag "builtin"
-  :custom
-  ;; Keep obsolete items for a month
-  (newsticker-keep-obsolete-items . t)
-  `(newsticker-obsolete-item-max-age . ,(* 30 86400))
-  ;; Sane behavior
-  (newsticker-automatically-mark-items-as-old . nil)
-  (newsticker-automatically-mark-visited-items-as-old . t)
-  ;; No logos
-  (newsticker-download-logos . nil)
-  (newsticker-enable-logo-manipulations . nil)
-  ;; Emacs async sucks
-  (newsticker-retrieval-method . 'extern)
-  ;; Improve the default display
-  (newsticker-treeview-listwindow-height . 20)
-  (newsticker-treeview-date-format . "%F %a, %H:%M  ")
-  (newsticker-url-list-defaults . nil)
-  (newsticker-url-list . '(("Planet Emacslife" "https://planet.emacslife.com/atom.xml")
-                           ("Mastering Emacs" "http://www.masteringemacs.org/feed/")
-                           ("Oremacs" "https://oremacs.com/atom.xml")
-                           ("EmacsCast" "https://pinecast.com/feed/emacscast")
-                           ("LWN" "https://lwn.net/headlines/rss"))))
-
-
 (leaf markdown-mode
   :straight t
   :commands (markdown-mode gfm-mode)
