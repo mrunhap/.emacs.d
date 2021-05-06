50d04a8514fc6290a24465abfe75890739c29e09
mrunhap <liubolovelife@gmail.com>
2021-05-06 17:59:05 +0800

    update

--- lisp/init-reader.el
+++ lisp/init-reader.el
@@ -2,21 +2,12 @@
 
 ;; TODO elfeed
 (straight-use-package 'markdown-mode)
-(straight-use-package '(go-translate :type git :host github :repo "lorniu/go-translate"))
+(straight-use-package '(multi-translate :type git :host github :repo "twlz0ne/multi-translate.el"))
 
 ;; markdown-mode
 (setq markdown-fontify-code-blocks-natively t)
 (add-hook 'markdown-mode-hook 'markdown-toggle-markup-hidding)
 
-;; go-translate
-;; FIXME
-(setq
- go-translate-token-current (cons 430675 2721866130)
- go-translate-inputs-function #'go-translate-inputes-current-or-prompt
- go-translate-base-url "https://translate.google.cn"
- go-translate-local-language "zh-CN")
-
-(autoload 'go-translate "go-translate" nil t)
-(autoload 'go-translate-popup "go-translate" nil t)
+;; multi-translate
 
 (provide 'init-reader)
