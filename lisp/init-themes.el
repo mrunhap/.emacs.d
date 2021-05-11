0eea191e4e4c938cab5fc6ff5a6a57a2453d9b40
mrunhap <liubolovelife@gmail.com>
2021-05-11 11:43:26 +0800

    update

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,11 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
-(straight-use-package '(nasy-theme    :type git :host github :repo "404cn/nasy-theme.el"))
 (straight-use-package '(lazycat-theme :type git :host github :repo "404cn/lazycat-theme"))
 (straight-use-package '(modus-theme   :type git :host github :repo "protesilaos/modus-themes"))
-(straight-use-package '(joker-theme   :type git :host github :repo "DogLooksGood/joker-theme"))
-
-(require 'joker-theme)
 
 ;;; Use window divider
 (window-divider-mode 1)
