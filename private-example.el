1644d5541d5e68d14eeb905d2e19be4dea121099
mrunhap <liubolovelife@gmail.com>
2021-05-12 20:09:36 +0800

    update

---
+++ private-example.el
@@ -0,0 +1,10 @@
+;;; -*- lexical-binding: t -*-
+
+(setq
+ ;; font
+ *font* "Operator Mono SSm Lig"
+ *font-cn* "WenQuanYi Micro Hei"
+ *font-unicode* "Apple Color Emoji"
+ *font-height* (cond ((eq system-type 'darwin) 150)
+                     (t 130))
+ footheme 'paperlike)
