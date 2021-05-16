64e20b50043cd7501b4886f29c6dc4f1bd54780b
mrunhap <liubolovelife@gmail.com>
2021-05-16 19:45:02 +0800

    update

--- early-init.el
+++ early-init.el
@@ -1,13 +1,13 @@
 ;; From centaur
 ;; Faster to disable these here (before they've been initialized)
 (push '(fullscreen . maximized) default-frame-alist)
-(push '(menu-bar-lines . 0) default-frame-alist)
 (push '(scroll-bar-mode . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 (when (featurep 'ns)
   (push '(ns-transparent-titlebar . t) default-frame-alist))
 (when (not (featurep 'ns))
-  (push '(tool-bar-lines . 0) default-frame-alist))
+  (push '(tool-bar-lines . 0) default-frame-alist)
+  (push '(menu-bar-lines . 0) default-frame-alist))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
