1b609436cd455028dce4ffb8e250f482975d543a
mrunhap <liubolovelife@gmail.com>
2021-05-18 20:20:15 +0800

    update

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -3,7 +3,8 @@
 (setq
  python-indent-offset 4
  python-shell-completion-native-enable nil
- python-shell-interpreter "python3")
+ python-shell-interpreter "python3"
+ python-indent-guess-indent-offset nil)
 
 (with-eval-after-load "python"
   (add-hook 'python-mode-hook 'eglot-ensure))
