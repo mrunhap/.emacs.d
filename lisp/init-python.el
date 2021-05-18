7d92219109447ee11b34d8f0ee64f700cc3633c3
mrunhap <liubolovelife@gmail.com>
2021-05-18 20:51:08 +0800

    update

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,12 +1,24 @@
 ;;; -*- lexical-binding: t -*-
 
+(straight-use-package 'elpy)
+(straight-use-package 'blacken)
+(straight-use-package 'live-py-mode)
+
+(with-eval-after-load "python"
+  ;;; live-py-mode
+  ;; pip install twisted ?
+  (define-key python-mode-map (kbd "C-c l") 'live-py-mode)
+  ;;; blacken - reformat python buffer
+  ;; format evere time you save
+  ;; pip install black
+  (add-hook 'python-mode-hook 'blacken-mode)
+  ;;; elpy
+  (add-hook 'python-mode-hook (lambda () (elpy-enable))))
+
 (setq
  python-indent-offset 4
  python-shell-completion-native-enable nil
  python-shell-interpreter "python3"
  python-indent-guess-indent-offset nil)
-
-(with-eval-after-load "python"
-  (add-hook 'python-mode-hook 'eglot-ensure))
 
 (provide 'init-python)
