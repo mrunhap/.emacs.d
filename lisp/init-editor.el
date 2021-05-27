8f4cc1551e13f1997da8d5f4a47f8daf9bc04c05
mrunhap <liubolovelife@gmail.com>
2021-05-27 20:40:25 +0800

    update try to fix

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -15,6 +15,53 @@
 (straight-use-package 'writeroom-mode)
 (straight-use-package 'pretty-hydra)
 
+;;; pretty-hydra
+(defun +change-theme(theme)
+  "Disable other themes and load new one"
+  (mapc #'disable-theme custom-enabled-themes)
+  (load-theme theme t)
+  (message "Loaded theme `%s'" theme))
+
+;; FIXME
+(pretty-hydra-define toggles-hydra (:title "Toggles" :quit-key "q")
+      ("Basic"
+       (("n" (if (fboundp 'display-line-numbers-mode)
+                 (display-line-numbers-mode (if display-line-numbers-mode -1 1))
+               (global-linum-mode (if global-linum-mode -1 1)))
+         "line number"
+         :toggle (or (bound-and-true-p display-line-numbers-mode) global-linum-mode))
+        ("e" electric-pair-mode "electric pair" :toggle t)
+        ("c" flyspell-mode "spell check" :toggle t)
+        ("s" prettify-symbols-mode "pretty symbol" :toggle t)
+        ("b" display-battery-mode "battery" :toggle t)
+        ("i" display-time-mode "time" :toggle t))
+       "Highlight"
+       (("h l" global-hl-line-mode "line" :toggle t)
+        ("h p" show-paren-mode "paren" :toggle t)
+        ("h r" rainbow-mode "rainbow" :toggle t)
+        ("h d" rainbow-delimiters-mode "delimiter" :toggle t)
+        ("h t" global-hl-todo-mode "todo" :toggle t))
+       "Program"
+       (("F" flymake-mode "flymake" :toggle t)
+        ("O" hs-minor-mode "hideshow" :toggle t)
+        ("u" subword-mode "subword" :toggle t)
+        ("W" which-function-mode "which function" :toggle t)
+        ("E" toggle-debug-on-error "debug on error" :toggle (default-value 'debug-on-error))
+        ("Q" toggle-debug-on-quit "debug on quit" :toggle (default-value 'debug-on-quit))
+        ("v" global-diff-hl-mode "gutter" :toggle t)
+        ("V" diff-hl-flydiff-mode "live gutter" :toggle t)
+        ("M" diff-hl-margin-mode "margin gutter" :toggle t)
+        ("D" diff-hl-dired-mode "dired gutter" :toggle t))
+       "Theme"
+       (("t s" (+change-theme 'spacemacs-light) "space-light" :toggle t)
+        ("t S" (+change-theme 'spacemacs-dark) "space-dark" :toggle t)
+        ("t j" (+change-theme 'joker) "joker" :toggle t)
+        ("t j" (+change-theme 'printed) "printed" :toggle t)
+        ("t j" (+change-theme 'storybook) "storybook" :toggle t)
+        ("t n" (+change-theme 'nasy) "nasy" :toggle t))))
+
+(global-set-key (kbd "C-h C-h") 'toggles-hydra/body)
+
 ;;; writeroom-mode
 (setq
  writeroom-fullscreen-effect 'maximized
