34ad1207084b97636f526c2e1b8b4044e8284d0c
mrunhap <liubolovelife@gmail.com>
2021-05-28 20:59:12 +0800

    update

--- private-example.el
+++ private-example.el
@@ -1,10 +1,8 @@
 ;;; -*- lexical-binding: t -*-
 
 (setq
- ;; font
- +font "Operator Mono SSm Lig"
- +font-cn "WenQuanYi Micro Hei"
- +font-unicode "Apple Color Emoji"
- +font-height (cond ((eq system-type 'darwin) 150)
-                     (t 130))
- +theme 'paperlike)
+ +font "PxPlus IBM VGA8"
+ +font-cn "Unifont"
+ +font-unicode "Unifont"
+ +font-height 130
+ +theme 'minidark)
