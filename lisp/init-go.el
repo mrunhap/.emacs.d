f16a02b44eff90686ead85d1b62fc27314139784
mrunhap <liubolovelife@gmail.com>
2021-05-29 14:22:22 +0800

    update

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -7,7 +7,39 @@
 (straight-use-package 'go-fill-struct)
 (straight-use-package 'go-impl)
 (straight-use-package 'go-tag)
+(straight-use-package 'go-playground)
 (straight-use-package '(flymake-golangci :type git :host gitlab :repo "shackra/flymake-golangci"))
+
+;; Install or update tools
+(defvar go--tools '("golang.org/x/tools/cmd/goimports"
+                    "golang.org/x/tools/gopls"
+                    "github.com/go-delve/delve/cmd/dlv"
+                    "github.com/josharian/impl"
+                    "github.com/cweill/gotests/..."
+                    "github.com/fatih/gomodifytags"
+                    "github.com/davidrjenni/reftools/cmd/fillstruct")
+  "All necessary go tools.")
+
+(defun go-update-tools ()
+  "Install or update go tools."
+  (interactive)
+  (unless (executable-find "go")
+    (user-error "Unable to find `go' in `exec-path'!"))
+
+  (message "Installing go tools...")
+  (let ((proc-name "go-tools")
+        (proc-buffer "*Go Tools*"))
+    (dolist (pkg go--tools)
+      (set-process-sentinel
+       (start-process proc-name proc-buffer "go" "get" "-u" "-v" pkg)
+       (lambda (proc _)
+         (let ((status (process-exit-status proc)))
+           (if (= 0 status)
+               (message "Installed %s" pkg)
+             (message "Failed to install %s: %d" pkg status))))))))
+
+;;; go-playground
+(autoload 'go-playground-mode "go-playground" nil t)
 
 ;;; go-mode
 (setq gofmt-command "goimports")
@@ -15,6 +47,9 @@
 (with-eval-after-load "go-mode"
   (with-eval-after-load "exec-path-from-shell"
     (exec-path-from-shell-copy-envs '("GOPATH" "GO111MODULE" "GOPROXY")))
+  ;; Try to install go tools if `gopls' is not found
+  (unless (executable-find "gopls")
+    (go-update-tools))
   (add-hook 'go-mode-hook 'eglot-ensure)
   (add-hook 'before-save-hook 'gofmt-before-save)
 
