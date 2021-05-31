f783399ba32a1ddf70e01d7f4d6ebbcb54d02657
mrunhap <liubolovelife@gmail.com>
2021-05-31 23:38:02 +0800

    hydra for spartparens

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -13,8 +13,48 @@
 (add-hook 'clojure-mode-hook 'smartparens-mode)
 (add-hook 'smartparens-mode-hook 'smartparens-strict-mode)
 
+(pretty-hydra-define smartparens-hydra (:title "smartparens" :quit-key "q")
+  ("Moving"
+   (("a" sp-beginning-of-sexp)
+    ("e" sp-end-of-sexp)
+    ("f" sp-forward-sexp)
+    ("b" sp-backward-sexp)
+    ("n" sp-down-sexp)
+    ("N" sp-backward-down-sexp)
+    ("p" sp-up-sexp)
+    ("P" sp-backward-up-sexp))
+   "Slurping & barfing"
+   (("h" sp-backward-slurp-sexp)
+    ("H" sp-backward-barf-sexp)
+    ("l" sp-forward-slurp-sexp)
+    ("L" sp-forward-barf-sexp))
+   "Wrapping"
+   (("R" sp-rewrap-sexp)
+    ("u" sp-unwrap-sexp)
+    ("U" sp-backward-unwrap-sexp)
+    ("(" sp-wrap-round)
+    ("{" sp-wrap-curly)
+    ("[" sp-wrap-square))
+   "Sexp juggling"
+   (("S" sp-split-sexp)
+    ("s" sp-splice-sexp)
+    ("r" sp-raise-sexp)
+    ("j" sp-join-sexp)
+    ("t" sp-transpose-sexp)
+    ("A" sp-absorb-sexp)
+    ("E" sp-emit-sexp)
+    ("o" sp-convolute-sexp))
+   "Destructive editing"
+   (("c" sp-change-inner :exit t)
+    ("C" sp-change-enclosing :exit t)
+    ("k" sp-kill-sexp)
+    ("K" sp-backward-kill-sexp)
+    ("w" sp-copy-sexp))))
+
 (with-eval-after-load "smartparens"
-  (require 'smartparens-config))
+  (require 'smartparens-config)
+  (global-set-key (kbd "C-c C-p") 'smartparens-hydra/body))
+
 
 ;;; clojure-mode
 (autoload 'clojure-mode "clojure-mode")
