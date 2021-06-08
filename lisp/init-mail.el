f194932de1f12a0271553aff925d3ec8148fe9dc
mrunhap <liubolovelife@gmail.com>
2021-06-08 20:23:00 +0800

    Add send mail

--- lisp/init-mail.el
+++ lisp/init-mail.el
@@ -18,4 +18,29 @@
 
 (autoload 'notmuch "notmuch" "notmuch mail" t)
 
+(defun +notmuch-toggle-deleted-tag()
+  "toggle deleted tag for message"
+  (interactive)
+  (if (member "deleted" (notmuch-show-get-tags))
+      (notmuch-show-tag (list "-deleted"))
+    (notmuch-show-tag (list "+deleted"))))
+
+(with-eval-after-load "notmuch"
+  (define-key notmuch-search-mode-map "d" #'+notmuch-toggle-deleted-tag))
+
+;;; sendmail and smtpmail
+;; TODO gpg encrypt gmail app password in ~/.authinfo
+(setq
+ user-full-name "Bo Liu"
+ user-mail-address "liubolovelife@gmail.com"
+ send-mail-function #'smtpmail-send-it
+ smtpmail-default-smtp-server "smtp.gmail.com"
+ smtpmail-smtp-server "smtp.gmail.com"
+ smtpmail-smtp-service 587
+ smptmail-stream-type 'ssl
+ smtpmail-smtp-user "liubolovelife@gmail.com")
+
+;; TODO add hydra to add header in message-mode
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Mail-Headers.html
+
 (provide 'init-mail)
