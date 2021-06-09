f4f974a87d99ffcaf08d3c958018f643d281f0b8
mrunhap <liubolovelife@gmail.com>
2021-06-09 19:36:15 +0800

    unbind godef-jump

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -68,6 +68,10 @@
     (go-update-tools))
   (add-hook 'go-mode-hook 'eglot-ensure)
   (add-hook 'before-save-hook 'gofmt-before-save)
+  ;; unbind godef-jump, it's too slow and this key for org journal
+  (add-hook 'go-mode-hook
+            (lambda()
+              (local-unset-key (kbd "C-c C-j"))))
 
   (add-hook 'go-test-mode-hook 'visual-line-mode)
 
