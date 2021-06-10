ac2733e92c023701a6982f5ec1ae3154a3df5cb1
mrunhap <liubolovelife@gmail.com>
2021-06-10 10:21:27 +0800

    update go

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -26,7 +26,6 @@
 
 ;; Install or update tools
 (defvar go--tools '("golang.org/x/tools/cmd/goimports"
-                    "golang.org/x/tools/gopls"
                     "github.com/go-delve/delve/cmd/dlv"
                     "github.com/josharian/impl"
                     "github.com/cweill/gotests/..."
@@ -36,23 +35,37 @@
                     "github.com/davidrjenni/reftools/cmd/fillstruct")
   "All necessary go tools.")
 
-(defun go-update-tools ()
-  "Install or update go tools."
-  (interactive)
-  (unless (executable-find "go")
-    (user-error "Unable to find `go' in `exec-path'!"))
+;; Do not use the -u flag for gopls, as it will update the dependencies to incompatible versions
+;; https://github.com/golang/tools/blob/master/gopls/doc/user.md#installation
+(defvar go--tools-no-update '("golang.org/x/tools/gopls@latest"))
 
-  (message "Installing go tools...")
-  (let ((proc-name "go-tools")
-        (proc-buffer "*Go Tools*"))
-    (dolist (pkg go--tools)
-      (set-process-sentinel
-       (start-process proc-name proc-buffer "go" "get" "-u" "-v" pkg)
-       (lambda (proc _)
-         (let ((status (process-exit-status proc)))
-           (if (= 0 status)
-               (message "Installed %s" pkg)
-             (message "Failed to install %s: %d" pkg status))))))))
+
+(defun go-update-tools ()
+    "Install or update go tools."
+    (interactive)
+    (unless (executable-find "go")
+      (user-error "Unable to find `go' in `exec-path'!"))
+
+    (message "Installing go tools...")
+    (let ((proc-name "go-tools")
+          (proc-buffer "*Go Tools*"))
+      (dolist (pkg go--tools-no-update)
+        (set-process-sentinel
+         (start-process proc-name proc-buffer "go" "get" "-v" pkg)
+         (lambda (proc _)
+           (let ((status (process-exit-status proc)))
+             (if (= 0 status)
+                 (message "Installed %s" pkg)
+               (message "Failed to install %s: %d" pkg status))))))
+
+      (dolist (pkg go--tools)
+        (set-process-sentinel
+         (start-process proc-name proc-buffer "go" "get" "-u" "-v" pkg)
+         (lambda (proc _)
+           (let ((status (process-exit-status proc)))
+             (if (= 0 status)
+                 (message "Installed %s" pkg)
+               (message "Failed to install %s: %d" pkg status))))))))
 
 ;;; go-playground
 (autoload 'go-playground-mode "go-playground" nil t)
