694239378d5f47aa2e2877b238392d1a3ae7ee85
mrunhap <liubolovelife@gmail.com>
2021-06-13 11:58:20 +0800

    add pulse support

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -14,6 +14,25 @@
                  'tree-sitter-langs
                  'eglot)
 
+;; pulse current line
+(defun pulse-region (beg end &rest _)
+  "Pulse the current region."
+  (pulse-momentary-highlight-region beg end))
+(defun pulse-line (&rest _)
+  "Pulse the current line."
+  (pulse-momentary-highlight-one-line (point)))
+(defun recenter-and-pulse (&rest _)
+  "Recenter and pulse the current line."
+  (recenter)
+  (pulse-line))
+
+(advice-add #'xref-find-definitions :after #'recenter-and-pulse)
+(advice-add #'xref-find-definitions-at-mouse :after #'recenter-and-pulse)
+(advice-add #'xref-pop-marker-stack :after #'recenter-and-pulse)
+(add-hook 'dump-jump-after-jump-hook 'recenter-and-pulse)
+(add-hook 'bookmark-after-jump-hook 'recenter-and-pulse)
+(add-hook 'imenu-after-jump-hook 'recenter-and-pulse)
+
 ;;; tree-sitter
 ;; (add-hook 'go-mode-hook 'tree-sitter-mode)
 ;; (add-hook 'go-mode-hook 'tree-sitter-hl-mode)
@@ -25,7 +44,7 @@
 (global-set-key (kbd "C-c b") 'devdocs-lookup)
 
 ;; docstr
-;; FIXME go-mode, don't know how to use
+;; FIXME not work with meow in go mode
 (add-hook 'prog-mode-hook (lambda () (docstr-mode 1)))
 
 ;;; flymake
