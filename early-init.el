94f47c9ffe933435949dd0101c00be61b9ead481
mrunhap <liubolovelife@gmail.com>
2021-07-08 10:28:32 +0800

    update

--- early-init.el
+++ early-init.el
@@ -1,3 +1,14 @@
+(setq
+ ;; Do not initialise the package manager.  This is done in `init.el'.
+ package-enable-at-startup nil
+ ;; Resizing the Emacs frame can be a terribly expensive part of changing the
+ ;; font. By inhibiting this, we easily halve startup times with fonts that are
+ ;; larger than the system default.
+ frame-inhibit-implied-resize t
+ ;; After startup `gcmh' will reset this.
+ gc-cons-threshold most-positive-fixnum
+ gc-cons-percentage 0.6)
+
 ;; Faster to disable these here (before they've been initialized)
 (push '(fullscreen . maximized) default-frame-alist)
 (push '(scroll-bar-mode . 0) default-frame-alist)
@@ -10,14 +21,3 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
-
-(setq
- ;; Do not initialise the package manager.  This is done in `init.el'.
- package-enable-at-startup nil
- ;; Resizing the Emacs frame can be a terribly expensive part of changing the
- ;; font. By inhibiting this, we easily halve startup times with fonts that are
- ;; larger than the system default.
- frame-inhibit-implied-resize t
- ;; After startup `gcmh' will reset this.
- gc-cons-threshold most-positive-fixnum
- gc-cons-percentage 0.6)
