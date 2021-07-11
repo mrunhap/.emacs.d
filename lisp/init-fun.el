8b3a6297320236e0637065bd319b312c07620a08
mrunhap <liubolovelife@gmail.com>
2021-07-11 21:38:17 +0800

    add eaf

--- lisp/init-fun.el
+++ lisp/init-fun.el
@@ -15,4 +15,19 @@
              :host github
              :repo "SpringHan/netease-cloud-music.el"))
 
+(eat-package emacs-application-framework
+  :straight (emacs-application-framework
+             :type git
+             :host github
+             :repo "manateelazycat/emacs-application-framework"
+             :files ("*.el" "*.py" "core" "app"))
+  :init
+  (eat-package s :straight t)
+  (eat-package ctable :straight t)
+  (eat-package epc :straight t)
+  (eat-package deferred :straight t)
+  (setq eaf-browser-continue-where-left-off t)
+  :config
+  (setq eaf-browser-enable-adblocker "true"))
+
 (provide 'init-fun)
