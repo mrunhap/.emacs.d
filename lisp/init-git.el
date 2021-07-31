e664e0096e7cfe10bdcda408e640e6183ce00b86
mrunhap <liubolovelife@gmail.com>
2021-07-31 18:08:32 +0800

    make diff-hl work in tui

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -9,7 +9,18 @@
   :commands diff-hl-mode
   :hook
   ((prog-mode-hook conf-mode-hook) . diff-hl-mode)
-  (dired-mode-hook . diff-hl-dired-mode))
+  (dired-mode-hook . diff-hl-dired-mode)
+  :config
+  ;; Highlight on-the-fly
+  (diff-hl-flydiff-mode 1)
+
+  (unless (display-graphic-p)
+    ;; Fall back to the display margin since the fringe is unavailable in tty
+    (diff-hl-margin-mode 1)
+    ;; Avoid restoring `diff-hl-margin-mode'
+    (with-eval-after-load 'desktop
+      (add-to-list 'desktop-minor-mode-table
+                   '(diff-hl-margin-mode nil)))))
 
 (eat-package hl-todo
   :straight t
