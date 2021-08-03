da588f649eaf84253ad76c42d03296fd677cfd86
mrunhap <liubolovelife@gmail.com>
2021-08-03 11:04:34 +0800

    update markdown mode

--- lisp/init-spcfile.el
+++ lisp/init-spcfile.el
@@ -6,7 +6,22 @@
 
 (eat-package docker-compose-mode :straight t)
 (eat-package dockerfile-mode :straight t)
-(eat-package markdown-mode :straight t)
+
+(eat-package markdown-mode
+  :straight t
+  :mode ("\\.md\\" . gfm-mode)
+  :init
+  (setq markdown-enable-wiki-links t
+        markdown-italic-underscore t
+        markdown-asymmetric-header t
+        markdown-make-gfm-checkboxes-buttons t
+        markdown-gfm-uppercase-checkbox t
+        markdown-fontify-code-blocks-natively t))
+
+(eat-package markdown-toc
+  :straight t
+  :config
+  (define-key markdown-mode-command-map (kbd "r") #'markdown-toc-generate-or-refresh-toc))
 
 (eat-package fish-mode
   :straight t
