e81570956851885805528e973125f55cad0e9cac
mrunhap <liubolovelife@gmail.com>
2021-08-04 09:31:02 +0800

    update

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -57,7 +57,10 @@
   (setq python-indent-offset 4
         python-shell-completion-native-enable nil
         python-shell-interpreter "python3"
-        python-indent-guess-indent-offset nil))
+        python-indent-guess-indent-offset nil)
+  :config
+  (with-eval-after-load 'exec-path-from-shell
+    (exec-path-from-shell-copy-env "PYTHONPATH")))
 
 (eat-package conda
   :straight t
