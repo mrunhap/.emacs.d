b4ed9a915fbfd63a91bfbd517db24cc7549658be
mrunhap <liubolovelife@gmail.com>
2021-08-12 16:32:28 +0800

    telega open img in tui

--- lisp/init-telega.el
+++ lisp/init-telega.el
@@ -16,6 +16,7 @@
         ;; set date format for old messages
         telega-old-date-format "%Y/%M/%D"
         telega-proxies +telega-proxy)
+
   ;; pgtk emacs does not support copying image from clipboard, a simple workaround
   (define-advice telega-chatbuf-attach-clipboard (:override (doc-p) yang)
     "Attach clipboard image to the chatbuf as photo.
@@ -33,10 +34,22 @@ If `\\[universal-argument]' is given, then attach clipboard as document."
                           (error "No image in CLIPBOARD"))
                       nil tmpfile nil 'quiet))
       (telega-chatbuf--attach-tmp-photo tmpfile doc-p)))
+
+  (defun +telega-open-file (file)
+    (cond
+     ((member (downcase (file-name-extension file)) '("png" "jpg" "gif" "jpeg"))
+      (start-process "telega-open-photo" nil "/sbin/imv" file))
+     ((member (downcase (file-name-extension file)) '("mp4"))
+      (start-process "telega-open-video" nil "/sbin/mpv" file))
+     (t
+      (find-file file))))
   :hook
   (telega-msg-ignore-predicates . telega-msg-from-blocked-sender-p)
   (telega-chat-mode . #'yas-minor-mode-on)
   :config
+  (when (not (display-graphic-p))
+    (setq telega-open-message-as-file '(photo video)
+          telega-open-file-function '+telega-open-file))
   ;; syntax highlighting in telega code
   (require 'telega-mnz)
   (global-telega-mnz-mode 1))
