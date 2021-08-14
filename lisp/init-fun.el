7b2b2e8ec3116bf7ae70d7cfe3104a6749727443
mrunhap <liubolovelife@gmail.com>
2021-08-14 23:26:24 +0800

    update eaf config

--- lisp/init-fun.el
+++ lisp/init-fun.el
@@ -11,7 +11,9 @@
   (eat-package ctable :straight t)
   (eat-package epc :straight t)
   (eat-package deferred :straight t)
-  (setq eaf-browser-continue-where-left-off t)
+  (setq eaf-browser-continue-where-left-off t
+        eaf-kill-process-after-last-buffer-closed nil
+        eaf-start-python-process-when-require t)
   :config
   (setq eaf-browser-enable-adblocker "true"))
 
