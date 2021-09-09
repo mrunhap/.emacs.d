8f91e0057f289f0e5c709c782b12b10f2fad8b98
mrunhap <liubolovelife@gmail.com>
2021-09-09 19:51:09 +0800

    update mail

--- lisp/init-mail.el
+++ lisp/init-mail.el
@@ -7,15 +7,6 @@
   :straight t
   :commands notmuch
   :init
-  ;; TODO gpg encrypt gmail app password in ~/.authinfo
-  (setq user-full-name "Liu Bo"
-        user-mail-address "liubolovelife@gmail.com"
-        send-mail-function #'smtpmail-send-it
-        smtpmail-default-smtp-server "smtp.gmail.com"
-        smtpmail-smtp-server "smtp.gmail.com"
-        smtpmail-smtp-service 587
-        smptmail-stream-type 'ssl
-        smtpmail-smtp-user "liubolovelife@gmail.com")
   (setq notmuch-show-logo nil
         notmuch-search-oldest-first nil
         notmuch-search-result-format '(("date" . "%12s ")
@@ -24,5 +15,26 @@
                                        ("subject" . "%s ")
                                        ("tags" . "(%s)"))
         notmuch-show-empty-searches t))
+
+(eat-package message
+  :hook (message-mode-hook . auto-fill-mode)
+  :init
+  (setq user-full-name "Liu Bo"
+        user-mail-address "liubolovelife@gmail.com"
+        message-kill-buffer-on-exit t
+        message-mail-alias-type 'ecomplete
+        message-send-mail-function #'message-use-send-mail-function
+        message-signature user-full-name))
+
+(eat-package sendmail
+  :init
+  (setq send-mail-function #'smtpmail-send-it))
+
+(eat-package smtpmail
+  :init
+  (setq smtpmail-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-user user-mail-address
+        smtpmail-smtp-service 587
+        smptmail-stream-type 'ssl))
 
 (provide 'init-mail)
