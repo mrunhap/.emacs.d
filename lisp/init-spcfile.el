c4562abb82767c5b12f8e29961e49c443ec80ba4
mrunhap <liubolovelife@gmail.com>
2021-12-02 14:57:18 +0800

    gfm-view-mode is an readonly mode, should enable manually

--- lisp/init-spcfile.el
+++ lisp/init-spcfile.el
@@ -30,7 +30,7 @@
 
 (eat-package markdown-mode
   :straight t
-  :mode ("\\.md\\'" . gfm-view-mode)
+  :mode ("\\.md\\'" . gfm-mode)
   :init
   (setq markdown-enable-wiki-links t
         markdown-italic-underscore t
