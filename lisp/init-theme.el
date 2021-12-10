d3e98ca84839b56c44af7b22aadc1ec44f4d635d
mrunhap <liubolovelife@gmail.com>
2021-12-10 14:21:51 +0800

    update nano theme tab-bar

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -79,8 +79,8 @@
     (set-face-attribute 'variable-pitch frame
                         :font variable-pitch-font-spec
                         :height 1.2)
-    (set-face-attribute 'fixed-pitch frame :font fixed-pitch-font-spec)
-    (set-face-attribute 'fixed-pitch-serif frame :font fixed-pitch-font-spec)
+    ;; (set-face-attribute 'fixed-pitch frame :font fixed-pitch-font-spec)
+    ;; (set-face-attribute 'fixed-pitch-serif frame :font fixed-pitch-font-spec)
     (set-face-attribute 'tab-bar frame :inherit 'variable-pitch :height 0.8)
     (set-face-attribute 'mode-line frame :inherit 'variable-pitch)
     (set-face-attribute 'mode-line-inactive frame :inherit 'variable-pitch)))
