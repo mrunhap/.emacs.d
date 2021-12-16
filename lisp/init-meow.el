637098e71fe272b473c600ccbe62020fca0a7f4e
mrunhap <liubolovelife@gmail.com>
2021-12-16 22:14:34 +0800

    use meow motion mode in some notmuch mode

--- lisp/init-meow.el
+++ lisp/init-meow.el
@@ -11,6 +11,10 @@
   (dolist (mode '(go-dot-mod-mode
                   diff-mode))
     (add-to-list 'meow-mode-state-list `(,mode . normal)))
+  (dolist (mode '(notmuch-hello-mode
+                  notmuch-search-mode
+                  notmuch-tree-mode))
+    (add-to-list 'meow-mode-state-list `(,mode . motion)))
   (meow-setup)
   :init
   (setq meow-esc-delay 0.001)
