6c82dceb0cef3d4a6777ac562145ef383225b1d4
mrunhap <liubolovelife@gmail.com>
2022-02-07 10:38:11 +0800

    set utf-8

--- lisp/init-straight.el
+++ lisp/init-straight.el
@@ -121,23 +121,10 @@
                             (unless (frame-focus-state)
                               (garbage-collect))))))
 
-;; Encoding
-;; UTF-8 as the default coding system
-(when (fboundp 'set-charset-priority)
-  (set-charset-priority 'unicode))
-;; Explicitly set the prefered coding systems to avoid annoying prompt
-;; from emacs (especially on Microsoft Windows)
-(prefer-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-(set-language-environment 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-buffer-file-coding-system 'utf-8)
-(set-clipboard-coding-system 'utf-8)
-(set-file-name-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(modify-coding-system-alist 'process "*" 'utf-8)
+;; From DoomEmacs
+;; Contrary to what many Emacs users have in their configs, you don't need
+;; more than this to make UTF-8 the default coding system:
+(set-language-environment "UTF-8")
 
 ;; Speed up startup
 (setq auto-mode-case-fold nil)
