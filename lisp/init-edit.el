7f80e9da8cad8256c3f2dbb1613a9b51e94b33c6
mrunhap <liubolovelife@gmail.com>
2022-02-18 15:38:31 +0800

    don't custom ace-window font

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -172,11 +172,7 @@
   ace-window
   :init
   (setq aw-keys '(?a ?o ?e ?u ?i)
-        aw-scope 'frame)
-  (custom-set-faces
-   '(aw-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 2.0))))
-   '(aw-minibuffer-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 1.0))))
-   '(aw-mode-line-face ((t (:inherit mode-line-emphasis :bold t))))))
+        aw-scope 'frame))
 
 (eat-package popper
   :straight t
