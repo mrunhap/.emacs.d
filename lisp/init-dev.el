08590a8ee1a1964dbb4bace4a2cb1133b58e8edc
mrunhap <liubolovelife@gmail.com>
2022-03-20 20:24:40 +0800

    bind eglot-find-implementation

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -170,6 +170,7 @@
   (define-key eglot-mode-map (kbd "M-RET") 'eglot-code-actions)
   (define-key eglot-mode-map (kbd "C-c r") 'eglot-rename)
   (define-key eglot-mode-map (kbd "C-c h") 'eldoc)
+  (define-key eglot-mode-map (kbd "M-'") 'eglot-find-implementation)
   (add-to-list 'eglot-server-programs
                '(sql-mode . ("sqls" "-config" "~/.config/sqls/config.yaml")))
   (add-to-list 'eglot-server-programs
