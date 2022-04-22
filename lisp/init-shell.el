dffa6c20abcc535add88e85f57fc2bf529dabd1c
mrunhap <liubolovelife@gmail.com>
2022-04-23 01:26:31 +0800

    update shell config

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -1,5 +1,82 @@
 ;;; -*- lexical-binding: t -*-
 
+(defun term-mode-common-init ()
+  "The common initialization procedure for term/shell."
+  (setq-local scroll-margin 0)
+  (setq-local truncate-lines t)
+  (setq-local global-hl-line-mode nil))
+
+(eat-package term
+  :hook
+  (term-mode-hook . (term-mode-prompt-regexp-setup term-mode-common-init))
+  :init
+  (defun term-mode-prompt-regexp-setup ()
+    "Setup `term-prompt-regexp' for term-mode."
+    (setq-local term-prompt-regexp "^[^#$%>\n]*[#$%>] *")))
+
+(eat-package eshell
+  :hook
+  (eshell-mode-hook . (lambda ()
+                        (term-mode-common-init)
+                        ;; Eshell is not fully functional
+                        (setenv "PAGER" "cat")))
+  :config
+  ;; Prevent accident typing
+  (defalias 'eshell/vi 'find-file)
+  (defalias 'eshell/vim 'find-file)
+
+  (defun eshell/bat (file)
+    "cat FILE with syntax highlight."
+    (with-temp-buffer
+      (insert-file-contents file)
+      (let ((buffer-file-name file))
+        (delay-mode-hooks
+          (set-auto-mode)
+          (font-lock-ensure)))
+      (buffer-string)))
+
+  (defun eshell/f (filename &optional dir)
+    "Search for files matching FILENAME in either DIR or the
+current directory."
+    (let ((cmd (concat
+                (executable-find "find")
+                " " (or dir ".")
+                "      -not -path '*/.git*'"
+                " -and -not -path 'build'"    ;; the cmake build directory
+                " -and"
+                " -type f"
+                " -and"
+                " -iname '*" filename "*'")))
+      (eshell-command-result cmd)))
+
+  (defun eshell/z ()
+    "cd to directory with completion."
+    (let ((dir (completing-read "Directory: " (ring-elements eshell-last-dir-ring) nil t)))
+      (eshell/cd dir)))
+
+  (defun eshell-prompt ()
+    "Prompt for eshell."
+    (concat
+     (propertize user-login-name 'face 'font-lock-keyword-face)
+     "@"
+     "Noobmaster "
+     (if (equal (eshell/pwd) "~")
+         "~"
+       (abbreviate-file-name (eshell/pwd)))
+     " "
+     (if-let* ((vc (ignore-errors (vc-responsible-backend default-directory)))
+               (br (car (vc-git-branches))))
+         (concat (propertize "(" 'face 'success)
+                 (format "%s" vc)
+                 (propertize ")" 'face 'success)
+                 (propertize "-" 'face 'font-lock-string-face)
+                 (propertize "[" 'face 'success)
+                 (propertize br 'face 'font-lock-constant-face)
+                 (propertize "]" 'face 'success)
+                 " ")
+       "")
+     "% ")))
+
 ;; The interactive shell.
 ;;
 ;; It can be used as a `sh-mode' REPL.
@@ -7,14 +84,8 @@
 ;; `shell' is recommended to use over `tramp'.
 (eat-package shell
   :hook
-  (comint-mode-hook
-   . (my-comint-init revert-tab-width-to-default))
+  (shell-mode-hook . (term-mode-common-init revert-tab-width-to-default))
   :init
-  (defun my-comint-init ()
-    (setq-local
-     comint-input-ignoredups t
-     comint-process-echoes t))
-
   (defun shell-toggle ()
     "Toggle a persistent shell popup window.
 If popup is visible but unselected, select it.
@@ -33,12 +104,6 @@ If popup is focused, kill it."
   (defun revert-tab-width-to-default ()
     "Revert `tab-width' to default value."
     (setq-local tab-width 8)))
-
-(eat-package eshell)
-
-(eat-package term
-  :init
-  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"))
 
 (eat-package vterm
   :straight t
