29875435d55ae8ca058181c6f455c08b6798385c
mrunhap <liubolovelife@gmail.com>
2022-05-15 19:44:40 +0800

    use fd in project-find-file, disable color output for tramp

--- lisp/init-builtin.el
+++ lisp/init-builtin.el
@@ -288,40 +288,50 @@
 
 ;;; project managent
 (eat-package project
-  :hook (project-find-functions . my/project-try-local)
   :init
-  ;; don't remember tramp project
-  (defun my/project-remember-advice (fn pr &optional no-write)
-    (let* ((remote? (file-remote-p (project-root pr)))
-           (no-write (if remote? t no-write)))
-      (funcall fn pr no-write)))
-  (advice-add 'project-remember-project :around
-              'my/project-remember-advice)
-
-  ;; `project'
-  (defun my/project-files-in-directory (dir)
-    "Use `fd' to list files in DIR."
-    (let* ((default-directory dir)
-           (localdir (file-local-name (expand-file-name dir)))
-           (command (format "fd -H -t f -0 . %s" localdir)))
-      (project--remote-file-names
-       (sort (split-string (shell-command-to-string command) "\0" t)
-             #'string<))))
-
-  ;; this cause wrong encoding when project-find-file FIXME
-  ;; (when (executable-find "fd")
-  ;;   (cl-defmethod project-files ((project (head local)) &optional dirs)
-  ;;     "Override `project-files' to use `fd' in local projects."
-  ;;     (mapcan #'my/project-files-in-directory
-  ;;             (or dirs (list (project-root project))))))
-
-  (defun +project-name ()
+  (defun eat/project-name ()
     (file-name-nondirectory
      (directory-file-name
       (project-root
        (project-current)))))
 
-  (defun my/project-try-local (dir)
+  (defun eat/project-info ()
+    (interactive)
+    (message "%s" (project-current t)))
+
+  (defun eat/add-dot-project ()
+    (interactive)
+    (let* ((root-dir (read-directory-name "Root: "))
+           (f (expand-file-name ".project" root-dir)))
+      (message "Create %s..." f)
+      (make-empty-file f)))
+
+  ;; do not remember tramp project
+  (defun eat/project-remember-advice (fn pr &optional no-write)
+    (let* ((remote? (file-remote-p (project-root pr)))
+           (no-write (if remote? t no-write)))
+      (funcall fn pr no-write)))
+  (advice-add 'project-remember-project :around
+              'eat/project-remember-advice)
+
+  :config
+  (defun eat/project-files-in-directory (dir)
+    "Use `fd' to list files in DIR."
+    (let* ((default-directory dir)
+           (localdir (file-local-name (expand-file-name dir)))
+           (command (format "fd -c never -H -t f -0 . %s" localdir)))
+      (project--remote-file-names
+       (sort (split-string (shell-command-to-string command) "\0" t)
+             #'string<))))
+
+  ;; use fd in `project-find-file'
+  (when (executable-find "fd")
+    (cl-defmethod project-files ((project (head local)) &optional dirs)
+      "Override `project-files' to use `fd' in local projects."
+      (mapcan #'eat/project-files-in-directory
+              (or dirs (list (project-root project))))))
+
+  (defun eat/project-try-local (dir)
     "Determine if DIR is a non-Git project."
     (catch 'ret
       (let ((pr-flags '((".project")
@@ -332,7 +342,8 @@
             (when-let ((root (locate-dominating-file dir f)))
               (throw 'ret (cons 'local root))))))))
   (cl-defmethod project-root ((project (head local)))
-    (cdr project)))
+    (cdr project))
+  (add-to-list 'project-find-functions #'eat/project-try-local t))
 
 (eat-package tab-bar
   :init
