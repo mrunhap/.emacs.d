0017e61662bbe43b899bce9d5427a614740e14c7
mrunhap <liubolovelife@gmail.com>
2022-09-06 16:59:05 +0800

    remap move-begining-of-line

--- lisp/init-eat.el
+++ lisp/init-eat.el
@@ -767,6 +767,31 @@ ARGS.
 ;; this will stuck emacs
 (global-unset-key (kbd "C-h h"))
 
+;; http://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+(global-set-key [remap move-beginning-of-line] #'smarter-move-beginning-of-line)
+
 
 ;;; Built-in packages
 (eat-package pulse
@@ -1183,6 +1208,7 @@ No tab will created if the command is cancelled."
   :config
   (define-key smerge-mode-map (kbd "C-c r") #'smerge-refine)
   (define-key smerge-mode-map (kbd "C-c c") #'smerge-keep-current)
+  (define-key smerge-mode-map (kbd "C-c a") #'smerge-keep-all)
   (define-key smerge-mode-map (kbd "C-c n") #'smerge-next)
   (define-key smerge-mode-map (kbd "C-c p") #'smerge-prev)
   (define-key smerge-mode-map (kbd "C-c l") #'smerge-keep-lower)
