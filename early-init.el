73fc5321c815d9b3f1b3d3588f59e8b74be0dde9
mrunhap <liubolovelife@gmail.com>
2024-04-10 11:10:33 +0800

    add idle-job-*

--- early-init.el
+++ early-init.el
@@ -32,10 +32,17 @@
                                          old-file-name-handler-alist))))
             t))
 
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
 ;; Faster to disable these here (before they've been initialized)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+(when (featurep 'ns)
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
 
 ;; Disable native compilation
 (setq native-comp-jit-compilation nil)
--- lisp/init-package.el
+++ lisp/init-package.el
@@ -16,4 +16,58 @@
         (package-refresh-contents))
       (package-install pkg))))
 
+
+;; https://github.com/PythonNut/quark-emacs/blob/dev/modules/config-package.el
+(defvar idle-jobs nil
+  "Symbols which need to be autoloaded.")
+
+(defvar idle-job-timer (run-with-idle-timer 0.1 t 'idle-job-run-next))
+
+(defun idle-job-run-next ()
+  "Load symbols from `idle-require-symbols' until input occurs."
+  (while (and idle-jobs
+              (not (input-pending-p)))
+    (cl-letf* ((old-load (symbol-function #'load))
+               ((symbol-function #'load)
+                (lambda (file &optional noerror _nomessage &rest args)
+                  (apply old-load
+                         file
+                         noerror
+                         (not (eq debug-on-error 'startup))
+                         args))))
+      (with-demoted-errors "Idle job error: %s"
+        (funcall (pop idle-jobs))))))
+
+(defun idle-job-add-require (sym &optional append)
+  (cl-letf ((fun (lambda ()
+                   (let ((start-time (current-time))
+                         (verbose debug-on-error))
+                     (unless (require sym nil t)
+                       (message "failed to load %s" sym))
+
+                     (when verbose
+                       (message "%.3f loaded %s"
+                                (float-time (time-subtract
+                                             (current-time)
+                                             start-time))
+                                sym))))))
+    (if append
+        (setq idle-jobs (append idle-jobs (list fun)))
+      (push fun idle-jobs))))
+
+(defun idle-job-add-function (sym &optional append)
+  (cl-letf ((fun (lambda ()
+                   (let ((start-time (current-time))
+                         (verbose debug-on-error))
+                     (funcall sym)
+                     (when verbose
+                       (message "%.3f ran %S"
+                                (float-time (time-subtract
+                                             (current-time)
+                                             start-time))
+                                sym))))))
+    (if append
+        (setq idle-jobs (append idle-jobs (list fun)))
+      (push fun idle-jobs))))
+
 (provide 'init-package)
