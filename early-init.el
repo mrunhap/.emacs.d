badd46b6b382d3b2066676ad6394ba479138c253
mrunhap <liubolovelife@gmail.com>
2024-04-10 11:18:17 +0800

    update osx

--- early-init.el
+++ early-init.el
@@ -41,8 +41,6 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-(when (featurep 'ns)
-  (push '(ns-transparent-titlebar . t) default-frame-alist))
 
 ;; Disable native compilation
 (setq native-comp-jit-compilation nil)
