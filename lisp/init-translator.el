129ad6daa76f07b649657fad6df37c877f486586
mrunhap <liubolovelife@gmail.com>
2024-07-31 11:48:22 +0800

    Remove chatgpt from translator

--- lisp/init-translator.el
+++ lisp/init-translator.el
@@ -1,23 +1,21 @@
 ;;; -*- lexical-binding: t -*-
 
-;; go-translate
+;;; go-translate
 ;;
 ;; Write to ~/.authinfo
 ;; machine api.deepl.com login auth-key password ****
 ;; machine api.openai.com login apikey password ****
 (install-package 'go-translate)
 
-(setq gt-langs '(en zh)
-      gt-chatgpt-model "gpt-3.5-turbo-16k")
+(setq gt-langs '(en zh))
 
 (with-eval-after-load 'go-translate
   (setq my/gt-engine
-        (append (when (auth-source-search :host "api.openai-sb.com" :user "apikey")
-                  (setq gt-chatgpt-host "https://api.openai-sb.com")
-                  (list (gt-chatgpt-engine)))
-                (when (auth-source-search :host "api.deepl.com" :user "auth-key")
-                  (list (gt-deepl-engine)))
-                (list (gt-google-engine) (gt-bing-engine))))
+        (append
+         (when (auth-source-search :host "api.deepl.com" :user "auth-key")
+           (list (gt-deepl-engine)))
+         (list (gt-google-engine)
+               (gt-bing-engine))))
   ;; NOTE youdao 不支持分段翻译
   (setq gt-default-translator (gt-translator :engines my/gt-engine :render  (gt-buffer-render))))
 
@@ -33,7 +31,7 @@ Default to translate buffer, or select region."
 
 (add-hook 'gt-buffer-render-init-hook #'visual-fill-column-mode)
 
-;; fanyi
+;;; fanyi
 (install-package 'fanyi)
 
 (defun my/translate ()
