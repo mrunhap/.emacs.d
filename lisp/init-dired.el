9a395533797ec2c9126513604139edec7252fde6
mrunhap <liubolovelife@gmail.com>
2024-12-24 15:13:51 +0800

    reuse dirvish

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -3,31 +3,50 @@
 ;; NOETS: for subtree, press i in dired.
 
 ;; icon
-(install-package 'nerd-icons-dired)
-
-(defun my/dired-enable-icon ()
-  (unless (file-remote-p default-directory)
-    (nerd-icons-dired-mode 1)))
-
-(add-hook 'dired-mode-hook #'my/dired-enable-icon)
+;; (install-package 'nerd-icons-dired)
+;;
+;; (defun my/dired-enable-icon ()
+;;   (unless (file-remote-p default-directory)
+;;     (nerd-icons-dired-mode 1)))
+;;
+;; (add-hook 'dired-mode-hook #'my/dired-enable-icon)
 
 ;; rsyncc
 (install-package 'dired-rsync)
 (with-eval-after-load 'dired
   (define-key dired-mode-map (kbd "C-c C-r") 'dired-rsync))
 
-(install-package 'treemacs)
-(install-package 'treemacs-nerd-icons)
-(install-package 'treemacs-magit)
-(install-package 'treemacs-tab-bar)
+;;; dirvish
+(install-package 'dirvish)
+(add-hook 'after-init-hook #'dirvish-override-dired-mode)
 
-(setq treemacs-recenter-after-file-follow t)
+(setq dirvish-attributes '(vc-state subtree-state nerd-icons)
+      dirvish-header-line-height 20
+      dirvish-mode-line-height 20)
 
-(with-eval-after-load 'treemacs
-  (require 'treemacs-magit)
-  (require 'treemacs-tab-bar)
-  (require 'treemacs-nerd-icons)
-  (treemacs-load-theme "nerd-icons")
-  (treemacs-set-scope-type 'Tabs))
+(keymap-global-set "<f1>" #'dirvish-side)
+
+(with-eval-after-load 'dirvish
+  (dirvish-side-follow-mode)
+  (define-key dirvish-mode-map (kbd "TAB") #'dirvish-subtree-toggle)
+  (define-key dirvish-mode-map (kbd "<tab>") #'dirvish-subtree-toggle)
+  (define-key dirvish-mode-map (kbd "a") #'dirvish-quick-access)
+  (define-key dirvish-mode-map (kbd "f") #'dirvish-file-info-menu)
+  (define-key dirvish-mode-map (kbd "y") #'dirvish-yank-menu)
+  (define-key dirvish-mode-map (kbd "N") #'dirvish-narrow)
+  (define-key dirvish-mode-map (kbd "H") #'dirvish-history-jump)
+  (define-key dirvish-mode-map (kbd "s") #'dirvish-quicksort)
+  (define-key dirvish-mode-map (kbd "v") #'dirvish-vc-menu)
+  (define-key dirvish-mode-map (kbd "M-f") #'dirvish-history-go-forward)
+  (define-key dirvish-mode-map (kbd "M-b") #'dirvish-history-go-backward)
+  (define-key dirvish-mode-map (kbd "M-l") #'dirvish-ls-switches)
+  (define-key dirvish-mode-map (kbd "M-m") #'dirvish-mark-menu)
+  (define-key dirvish-mode-map (kbd "M-t") #'dirvish-layout-toggle)
+  (define-key dirvish-mode-map (kbd "M-s") #'dirvish-setup-menu)
+  (define-key dirvish-mode-map (kbd "M-e") #'dirvish-emerge-menu)
+  (define-key dirvish-mode-map (kbd "M-j") #'dirvish-fd-jump)
+  (define-key dirvish-mode-map (kbd "<mouse-1>") #'dirvish-subtree-toggle-or-open)
+  (define-key dirvish-mode-map (kbd "<mouse-2>") #'dired-mouse-find-file-other-window)
+  (define-key dirvish-mode-map (kbd "<mouse-3>") #'dired-mouse-find-file))
 
 ;;; init-dired ends here
